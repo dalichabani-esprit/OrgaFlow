8ba85135e5f0fa9b98536b004a29706ee43d4f4b
manelhomri4 <manelhomri5@gmail.com>
2025-02-14 22:45:30 +0100

    Finished CRUD demande et facture now gonna work on main to test eveything

--- src/main/java/services/ServiceDemande.java
+++ src/main/java/services/ServiceDemande.java
@@ -1,6 +1,100 @@
 package services;
+
 import interfaces.IService;
+import models.demande;
+import utils.MyDatabase;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ServiceDemande {
+public class ServiceDemande implements IService<demande> {
+    private Connection cnx;
 
+    public ServiceDemande() {
+        cnx = MyDatabase.getInstance().getConnection();
+    }
+
+    public void add(demande demande) {
+        String query = "INSERT INTO demande (type, description, demandeur_id, date_demande, statut) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstm.setString(1, demande.getType());
+            pstm.setString(2, demande.getDescription());
+            pstm.setInt(3, demande.getDemandeur_id());
+            pstm.setDate(4, new java.sql.Date(demande.getDate_demande().getTime()));
+            pstm.setString(5, demande.getStatut());
+
+            pstm.executeUpdate();
+            ResultSet rs = pstm.getGeneratedKeys();
+            if (rs.next()) {
+                demande.setId_demande(rs.getInt(1));
+            }
+            System.out.println("Demande ajoutée avec succès !");
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de l'ajout de la demande : " + e.getMessage());
+        }
+    }
+
+    public List<demande> getAll() {
+        List<demande> demandes = new ArrayList<>();
+        String query = "SELECT * FROM demande";
+        try {
+            Statement stm = cnx.createStatement();
+            ResultSet rs = stm.executeQuery(query);
+
+            while (rs.next()) {
+                demande d = new demande(
+                        rs.getInt("id_demande"),
+                        rs.getString("type"),
+                        rs.getString("description"),
+                        rs.getInt("demandeur_id"),
+                        rs.getDate("date_demande"),
+                        rs.getString("statut")
+                );
+                demandes.add(d);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la récupération des demandes : " + e.getMessage());
+        }
+        return demandes;
+    }
+
+    public void update(demande demande) {
+        String query = "UPDATE demande SET type=?, description=?, demandeur_id=?, date_demande=?, statut=? WHERE id_demande=?";
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query);
+            pstm.setString(1, demande.getType());
+            pstm.setString(2, demande.getDescription());
+            pstm.setInt(3, demande.getDemandeur_id());
+            pstm.setDate(4, new java.sql.Date(demande.getDate_demande().getTime()));
+            pstm.setString(5, demande.getStatut());
+            pstm.setInt(6, demande.getId_demande());
+
+            int rowsUpdated = pstm.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Demande mise à jour avec succès !");
+            } else {
+                System.out.println("Aucune demande trouvée avec cet ID !");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la mise à jour de la demande : " + e.getMessage());
+        }
+    }
+
+    public void delete(demande demande) {
+        String query = "DELETE FROM demande WHERE id_demande=?";
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query);
+            pstm.setInt(1, demande.getId_demande());
+
+            int rowsDeleted = pstm.executeUpdate();
+            if (rowsDeleted > 0) {
+                System.out.println("Demande supprimée avec succès !");
+            } else {
+                System.out.println("Aucune demande trouvée avec cet ID !");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppression de la demande : " + e.getMessage());
+        }
+    }
 }
--- src/main/java/services/ServiceFacture.java
+++ src/main/java/services/ServiceFacture.java
@@ -1,4 +1,103 @@
 package services;
+
 import interfaces.IService;
-public class ServiceFacture {
+import models.facture;
+import utils.MyDatabase;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceFacture implements IService<facture> {
+    private Connection cnx;
+
+    public ServiceFacture() {
+        cnx = MyDatabase.getInstance().getConnection();
+    }
+
+    public void add(facture facture) {
+        String query = "INSERT INTO facture (id_devis, id_demande, montant_final, date_facture, statut, destinataire_id) VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstm.setInt(1, facture.getId_devis());
+            pstm.setInt(2, facture.getId_demande());
+            pstm.setFloat(3, facture.getMontant_final());
+            pstm.setDate(4, facture.getDate_facture());
+            pstm.setString(5, facture.getStatut());
+            pstm.setInt(6, facture.getDestinataire_id());
+
+            pstm.executeUpdate();
+            ResultSet rs = pstm.getGeneratedKeys();
+            if (rs.next()) {
+                facture.setId_facture(rs.getInt(1));
+            }
+            System.out.println("Facture ajoutée avec succès !");
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de l'ajout de la facture : " + e.getMessage());
+        }
+    }
+
+    public List<facture> getAll() {
+        List<facture> factures = new ArrayList<>();
+        String query = "SELECT * FROM facture";
+        try {
+            Statement stm = cnx.createStatement();
+            ResultSet rs = stm.executeQuery(query);
+
+            while (rs.next()) {
+                facture f = new facture(
+                        rs.getInt("id_facture"),
+                        rs.getInt("id_devis"),
+                        rs.getInt("id_demande"),
+                        rs.getFloat("montant_final"),
+                        rs.getDate("date_facture"),
+                        rs.getString("statut"),
+                        rs.getInt("destinataire_id")
+                );
+                factures.add(f);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la récupération des factures : " + e.getMessage());
+        }
+        return factures;
+    }
+
+    public void update(facture facture) {
+        String query = "UPDATE facture SET id_devis=?, id_demande=?, montant_final=?, date_facture=?, statut=?, destinataire_id=? WHERE id_facture=?";
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query);
+            pstm.setInt(1, facture.getId_devis());
+            pstm.setInt(2, facture.getId_demande());
+            pstm.setFloat(3, facture.getMontant_final());
+            pstm.setDate(4, facture.getDate_facture());
+            pstm.setString(5, facture.getStatut());
+            pstm.setInt(6, facture.getDestinataire_id());
+            pstm.setInt(7, facture.getId_facture());
+
+            int rowsUpdated = pstm.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("Facture mise à jour avec succès !");
+            } else {
+                System.out.println("Aucune facture trouvée avec cet ID !");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la mise à jour de la facture : " + e.getMessage());
+        }
+    }
+
+    public void delete(facture facture) {
+        String query = "DELETE FROM facture WHERE id_facture=?";
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query);
+            pstm.setInt(1, facture.getId_facture());
+
+            int rowsDeleted = pstm.executeUpdate();
+            if (rowsDeleted > 0) {
+                System.out.println("Facture supprimée avec succès !");
+            } else {
+                System.out.println("Aucune facture trouvée avec cet ID !");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppression de la facture : " + e.getMessage());
+        }
+    }
 }
