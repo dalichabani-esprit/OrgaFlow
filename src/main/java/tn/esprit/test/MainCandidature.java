c47e526dd78b6b0a5a39c708cdfea687a45339ff
Rachid <AimeRachid.Traore@esprit.tn>
2025-02-16 16:55:13 +0100

    j'ai fait mon interface listviewCandidature mais le code ne fonctionne pas encore

--- src/main/java/tn/esprit/test/MainCandidature.java
+++ src/main/java/tn/esprit/test/MainCandidature.java
@@ -1,4 +1,41 @@
 package tn.esprit.test;
 
-public class MainCandidature {
-}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+    public class MainCandidature extends Application {
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage primaryStage) {
+            FXMLLoader loader= new FXMLLoader(getClass().getResource("/GestionCandidature.fxml"));
+            try {
+                Parent root =loader.load();
+                Scene scene = new Scene(root);
+                primaryStage.setScene(scene);
+                primaryStage.setTitle("hello from the other side ");
+                primaryStage.show();
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+    }
+
