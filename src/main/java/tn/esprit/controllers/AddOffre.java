90013f9e4535cd67568061820fb26d007eee20bf
Rachid <AimeRachid.Traore@esprit.tn>
2025-02-22 23:24:12 +0100

    interface modifyoffre ajoutée niveau de progression 30%

--- src/main/java/tn/esprit/controllers/AddOffre.java
+++ src/main/java/tn/esprit/controllers/AddOffre.java
@@ -3,7 +3,11 @@ package tn.esprit.controllers;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -11,12 +15,14 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import tn.esprit.interfaces.IService;
 import tn.esprit.models.Candidature;
 import tn.esprit.models.OffreEmploi;
 import tn.esprit.services.ServiceCandidature;
 import tn.esprit.services.ServiceOffreEmploi;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.ResourceBundle;
@@ -73,6 +79,18 @@ public class AddOffre implements Initializable {
 
 
         so.add(o);
+    }
+
+
+
+    @FXML
+    void retour(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/ShowOffre.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
 }
--- src/main/java/tn/esprit/controllers/ShowOffre.java
+++ src/main/java/tn/esprit/controllers/ShowOffre.java
@@ -92,4 +92,15 @@ public class ShowOffre implements Initializable {
         stage.show();
 
     }
+
+    @FXML
+    void sceneadd(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/AddOffre.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setMaximized(true);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
