1f8009c841bd0d96375429dc8ed3522423978f8e
Rachid <AimeRachid.Traore@esprit.tn>
2025-02-22 20:44:42 +0100

    interface Ajouter offre fonctionnel niveau de progression 20%

--- src/main/java/tn/esprit/controllers/AddOffre.java
+++ src/main/java/tn/esprit/controllers/AddOffre.java
@@ -1,4 +1,78 @@
 package tn.esprit.controllers;
 
-public class AddOffre {
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import tn.esprit.interfaces.IService;
+import tn.esprit.models.Candidature;
+import tn.esprit.models.OffreEmploi;
+import tn.esprit.services.ServiceCandidature;
+import tn.esprit.services.ServiceOffreEmploi;
+
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
+
+public class AddOffre implements Initializable {
+
+    @FXML
+    private ComboBox<String> cbdep;
+
+    @FXML
+    private ComboBox<String> cbstatus;
+
+    @FXML
+    private DatePicker dplimit;
+
+    @FXML
+    private DatePicker dppubli;
+
+    @FXML
+    private ProgressBar progression;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private VBox sidebar;
+
+    @FXML
+    private TextArea tfdesc;
+
+    @FXML
+    private TextField tftitre;
+
+    @FXML
+    private Button valider;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cbdep.setItems(FXCollections.observableArrayList("Ressources Humaines", "Formation", "Projet", "Facturation"));
+        cbstatus.setItems(FXCollections.observableArrayList("ouvert", "fermé"));
+
+    }
+    @FXML
+    IService<OffreEmploi> so = new ServiceOffreEmploi();
+
+    public void ajouterOffre(ActionEvent actionEvent) {
+        OffreEmploi o = new OffreEmploi();
+        o.setTitreOffre(tftitre.getText());
+        o.setDescriptionOffre(tfdesc.getText());
+        o.setDepartementOffre(cbdep.getValue());
+        o.setDate_publicationOffre(Date.valueOf(dppubli.getValue()));
+        o.setDate_limiteOffre(Date.valueOf(dplimit.getValue()));
+        o.setStatutOffre(cbstatus.getValue());
+
+
+        so.add(o);
+    }
+
 }
